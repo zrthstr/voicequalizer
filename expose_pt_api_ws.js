aeb462eb370fecbf218cb3a530bd66b0e4e75cf1
willclarktech <will@willclark.tech>
2017-04-27 22:31:27 +0200

    auto connect

--- expose_pt_api_ws.js
+++ expose_pt_api_ws.js
@@ -1,57 +1,63 @@
 function init() {
   document.myform.url.value = "ws://localhost:8080/"
   document.myform.inputtext.value = "Hello World!"
-  document.myform.disconnectButton.disabled = true;
+  document.myform.disconnectButton.disabled = true
+  doConnect()
 }
 function doConnect() {
-  websocket = new WebSocket(document.myform.url.value);
+  websocket = new WebSocket(document.myform.url.value)
   websocket.onopen = function(evt) {
     onOpen(evt)
-  };
+  }
   websocket.onclose = function(evt) {
     onClose(evt)
-  };
+  }
   websocket.onmessage = function(evt) {
     onMessage(evt)
-  };
+  }
   websocket.onerror = function(evt) {
     onError(evt)
-  };
+  }
 }
 function onOpen(evt) {
-  writeToScreen("connected\n");
-  document.myform.connectButton.disabled = true;
-  document.myform.disconnectButton.disabled = false;
+  writeToScreen("connected\n")
+  document.myform.connectButton.disabled = true
+  document.myform.disconnectButton.disabled = false
 }
 function onClose(evt) {
-  writeToScreen("disconnected\n");
-  document.myform.connectButton.disabled = false;
-  document.myform.disconnectButton.disabled = true;
+  writeToScreen("disconnected\n")
+  document.myform.connectButton.disabled = false
+  document.myform.disconnectButton.disabled = true
 }
 function onMessage(evt) {
-  writeToScreen("response: " + evt.data + '\n');
+  const w = document.getElementById('pt').contentWindow
+  w.alwaysVoice = false
+
+  writeToScreen(w.alwaysVoice)
+  writeToScreen('\n')
+  writeToScreen("response: " + evt.data + '\n')
 }
 function onError(evt) {
-  writeToScreen('error: ' + evt.data + '\n');
-  websocket.close();
-  document.myform.connectButton.disabled = false;
-  document.myform.disconnectButton.disabled = true;
+  writeToScreen('error: ' + evt.data + '\n')
+  websocket.close()
+  document.myform.connectButton.disabled = false
+  document.myform.disconnectButton.disabled = true
 }
 function doSend(message) {
-  writeToScreen("sent: " + message + '\n');
-  websocket.send(message);
+  writeToScreen("sent: " + message + '\n')
+  websocket.send(message)
 }
 function writeToScreen(message) {
   document.myform.outputtext.value += message
-  document.myform.outputtext.scrollTop = document.myform.outputtext.scrollHeight;
+  document.myform.outputtext.scrollTop = document.myform.outputtext.scrollHeight
 }
-window.addEventListener("load", init, false);
+window.addEventListener("load", init, false)
 function sendText() {
-  doSend(document.myform.inputtext.value);
+  doSend(document.myform.inputtext.value)
 }
 function clearText() {
-  document.myform.outputtext.value = "";
+  document.myform.outputtext.value = ""
 }
 function doDisconnect() {
-  websocket.close();
+  websocket.close()
 }
